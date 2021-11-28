ec8cb2aa0ad253aa1eeced51ad73186939acd084
Amel TRIKI <amel.triki.iset@gmail.com>
2021-11-28 18:53:42 +0100

    Exercice TP9 Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LivresComponent } from './livres/livres.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivresComponent
   ],
   imports: [
     BrowserModule
